2328027875ad43ea22eaafda0770cf824fd5a417
itsaeed0 <ashourisaeid78@gmail.com>
2024-12-05 19:56:02 +0330

    init

---
+++ fastfood-menu/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <head>
+        <div>
+          <div>
+            <h1>fast food</h1>
+          </div>
+        </div>
+      </head>
+    </>
+  )
+}
+
+export default App
